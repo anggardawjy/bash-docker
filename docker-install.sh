1bfaa2b46b553055c38bbc6bd4183f2e854b6a1d
Anggarda Wijaya <anggarda.wijaya410@gmail.com>
2024-08-06 03:11:11 +0700

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -2,11 +2,11 @@
 #author : github.com/anggardawjy
 #version : 0.1
 #about : docker-ce install ubuntu22.04
-sudo apt-get update
+sudo apt update
 sudo apt install curl -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/>
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-k>
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt-cache policy docker-ce
 sudo apt install docker-ce -y
