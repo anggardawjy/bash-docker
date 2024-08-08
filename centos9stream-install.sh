d710e71408b044908617c7e224104149102dd50f
Anggarda Wijaya <anggarda.wijaya410@gmail.com>
2024-08-08 14:24:48 +0700

    Add files via upload

---
+++ centos9stream-install.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#author : Anggarda Saputra Wijaya. anggarda.wijaya@outlook.com. github.com/anggardawjy
+#version : 0.1
+#about : centos 9 stream docker-ce install
+sudo dnf install nano -y
+sudo dnf install curl -y
+sudo dnf update -y
+sudo dnf install -y yum-utils device-mapper-persistent-data lvm2
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf repolist -v
+sudo dnf list docker-ce --showduplicates | sort -r
+sudo dnf install -y docker-ce --nobest
+sudo systemctl enable docker
+sudo systemctl status docker
